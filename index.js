d453ef7a1af966644b89cbe0867740460cac6dd6
taufik69 <taufik35-2915@diu.edu.bd>
2023-08-17 20:03:52 +0600

    unfullfil

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://www.youtube.com/");
+  await page.goto("https://www.youtube.com/watch?v=S67gyqnYHmI&t=588s");
   //   await page.setViewport({ width: 1480, height: 1080 });
   //   await page.screenshot({ path: "page.jpg", fullPage: true });
 
@@ -27,7 +27,21 @@ const puppeteer = require("puppeteer");
   //   const html = await page.content();
   //   console.log("html is : ", html);
 
-  const title = await page.evaluate(() => document.title);
-  console.log("title is : ", title);
+  //   const title = await page.evaluate((item) => document.body.innerText);
+  //   get all links
+  //   const allLinks = await page.evaluate(() =>
+  //     Array.from(document.querySelectorAll("a"), (e) => e.href)
+  //   );
+
+  // scraping data from specific option
+
+  //   let classbase = await page.evaluate(() =>
+  //     Array.from(document.querySelectorAll(".docMainContainer_gTbr"), (e) => ({
+  //       title: e,
+  //     }))
+  //   );
+
+  const random = await page.$$eval(".container", (e) => e.map(item)=>({}));
+  console.log("text is: ", random);
   await browser.close();
 })();
